cb2b0898a8d06fe2406013c030c5181dc117a020
Zech Sloan <patriotbob83@gmail.com>
2016-07-09 11:49:47 -0400

    Cleaning up build process

--- tasks/install.sh
+++ tasks/install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 WORKSPACE="$(pwd)"
-for dir in ../src; do
+for dir in src; do
     [ -d "${dir}" ] || continue
     cd "${dir}"
     dotnet restore
 done
 cd "${WORKSPACE}"
-for dir in ../test; do
+for dir in test; do
     [ -d "${dir}" ] || continue
     cd "${dir}"
     dotnet restore
--- tasks/test.sh
+++ tasks/test.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
+
 dotnet test test/ExpressionSerializer.Tests
